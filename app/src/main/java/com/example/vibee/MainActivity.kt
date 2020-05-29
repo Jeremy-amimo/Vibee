6f2575f1d439325824275941e7d879cba7dba8f0
jeremy Amimo <jeremyamimo402@gmail.com>
2020-05-29 04:01:58 +0300

    Vibe App

--- app/src/main/java/com/example/vibee/MainActivity.kt
+++ app/src/main/java/com/example/vibee/MainActivity.kt
@@ -1,67 +1,61 @@
 package com.example.vibee
 
-import Fragments.HomeFragment
-import Fragments.NotificationsFragment
-import Fragments.ProfileFragment
-import Fragments.SearchFragment
 import android.os.Bundle
-import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.example.vibee.Fragments.HomeFragment
+import com.example.vibee.Fragments.NotificationsFragment
+import com.example.vibee.Fragments.ProfileFragment
+import com.example.vibee.Fragments.SearchFragment
 
 class MainActivity : AppCompatActivity() {
-
-    internal var selectedFragment: Fragment? = null
 
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-                selectedFragment = HomeFragment()
+                moveToFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_search -> {
-                selectedFragment = SearchFragment()
+                moveToFragment(SearchFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_add_post -> {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_notifications -> {
-                selectedFragment = NotificationsFragment()
+                moveToFragment(NotificationsFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_profile -> {
-                selectedFragment = ProfileFragment()
+                moveToFragment(ProfileFragment())
+                return@OnNavigationItemSelectedListener true
             }
         }
 
-        if (selectedFragment != null){
-            supportFragmentManager.beginTransaction().replace(
-                R.id.fragment_container,
-                selectedFragment!!
-            ).commit()
-        }
         false
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
-        supportFragmentManager.beginTransaction().replace(
-            R.id.fragment_container,
-            HomeFragment()
-        ).commit()
+
+        moveToFragment(HomeFragment())
+    }
 
 
-
+    private fun moveToFragment(fragment: Fragment)
+    {
+        val fragmentTrans = supportFragmentManager.beginTransaction()
+        fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.commit()
     }
 }
--- app/src/main/java/com/example/vibee/SignUpActivity.kt
+++ app/src/main/java/com/example/vibee/SignUpActivity.kt
@@ -10,6 +10,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlin.collections.HashMap
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -17,47 +18,51 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+
         signin_link_btn.setOnClickListener {
             startActivity(Intent(this, SignInActivity::class.java))
         }
+
 
         signup_btn.setOnClickListener {
             CreateAccount()
         }
     }
 
-    private fun CreateAccount() {
+
+
+    private fun CreateAccount()
+    {
         val fullName = fullname_signup.text.toString()
         val userName = username_signup.text.toString()
         val email = email_signup.text.toString()
         val password = password_signup.text.toString()
 
         when{
-            TextUtils.isEmpty(fullName) -> Toast.makeText(this,"Full name is required",Toast.LENGTH_LONG).show()
-            TextUtils.isEmpty(userName) -> Toast.makeText(this,"Username is required",Toast.LENGTH_LONG).show()
-            TextUtils.isEmpty(email) -> Toast.makeText(this,"Email is required",Toast.LENGTH_LONG).show()
-            TextUtils.isEmpty(password) -> Toast.makeText(this,"Passord is required",Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(fullName) -> Toast.makeText(this, "full name is required.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(userName) -> Toast.makeText(this, "user name is required.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(email) -> Toast.makeText(this, "email is required.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this, "password is required.", Toast.LENGTH_LONG).show()
 
             else -> {
-
                 val progressDialog = ProgressDialog(this@SignUpActivity)
-                progressDialog.setTitle("Sign Up")
-                progressDialog.setMessage("Please Wait as we Confirm your credentials")
+                progressDialog.setTitle("SignUp")
+                progressDialog.setMessage("Please wait, this may take a while...")
                 progressDialog.setCanceledOnTouchOutside(false)
                 progressDialog.show()
-
 
                 val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
                 mAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
+                    .addOnCompleteListener{ task ->
                         if (task.isSuccessful)
                         {
                             saveUserInfo(fullName, userName, email, progressDialog)
                         }
-                        else {
+                        else
+                        {
                             val message = task.exception!!.toString()
-                            Toast.makeText(this,"Error: $message",Toast.LENGTH_LONG).show()
+                            Toast.makeText(this, "Error: $message", Toast.LENGTH_LONG).show()
                             mAuth.signOut()
                             progressDialog.dismiss()
                         }
@@ -66,24 +71,27 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
+
+
     private fun saveUserInfo(fullName: String, userName: String, email: String, progressDialog: ProgressDialog)
     {
-        val currentUserId = FirebaseAuth.getInstance().currentUser!!.uid
+        val currentUserID = FirebaseAuth.getInstance().currentUser!!.uid
         val usersRef: DatabaseReference = FirebaseDatabase.getInstance().reference.child("Users")
 
         val userMap = HashMap<String, Any>()
-        userMap ["uid"] = currentUserId
-        userMap ["fullname"] = currentUserId
-        userMap ["username"] = currentUserId
-        userMap ["email"] = currentUserId
-        userMap ["bio"] =  "Iam using Vibe it is such an amazing app"
-        userMap ["image"] = "gs://vibe-74de6.appspot.com/Default Images/profile.png"
+        userMap["uid"] = currentUserID
+        userMap["fullname"] = fullName.toLowerCase()
+        userMap["username"] = userName.toLowerCase()
+        userMap["email"] = email
+        userMap["bio"] = "hey i am using Coding Cafe Instagram Clone App."
+        userMap["image"] = "https://firebasestorage.googleapis.com/v0/b/vibe-74de6.appspot.com/o/Default%20Images%2Fprofile.png?alt=media&token=b4196f71-bb80-47e0-b2fd-6806d2ba7a75"
 
-        usersRef.child(currentUserId).setValue(userMap)
-            .addOnCompleteListener {task ->
-                if (task.isSuccessful){
+        usersRef.child(currentUserID).setValue(userMap)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful)
+                {
                     progressDialog.dismiss()
-                    Toast.makeText(this,"Account has been created successfully",Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Account has been created successfully.", Toast.LENGTH_LONG).show()
 
                     val intent = Intent(this@SignUpActivity, MainActivity::class.java)
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -93,7 +101,7 @@ class SignUpActivity : AppCompatActivity() {
                 else
                 {
                     val message = task.exception!!.toString()
-                    Toast.makeText(this,"Error: $message",Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Error: $message", Toast.LENGTH_LONG).show()
                     FirebaseAuth.getInstance().signOut()
                     progressDialog.dismiss()
                 }
